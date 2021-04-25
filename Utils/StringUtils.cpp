1911ccf337ceaf527bbd00f5a5b51f4124029224
Karine Farias <karine.farias@sap.com>
2021-04-25 11:32:34 -0300

    Server working

---
+++ Utils/StringUtils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Farias, Karine on 4/25/21.
+//
+
+#include "StringUtils.h"
---
+++ Utils/StringUtils.h
@@ -0,0 +1,14 @@
+//
+// Created by Farias, Karine on 4/25/21.
+//
+
+#ifndef SOCIALINE_STRINGUTILS_H
+#define SOCIALINE_STRINGUTILS_H
+
+
+class StringUtils {
+
+};
+
+
+#endif //SOCIALINE_STRINGUTILS_H
