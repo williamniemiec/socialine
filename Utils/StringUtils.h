3eb11b6141f47bbd7b90f10479ecc9a5b2ddfb6e
Karine Farias <karine.farias@sap.com>
2021-04-25 11:33:58 -0300

    Server working

--- Utils/StringUtils.h
+++ Utils/StringUtils.h
@@ -5,8 +5,31 @@
 #ifndef SOCIALINE_STRINGUTILS_H
 #define SOCIALINE_STRINGUTILS_H
 
+#include <iostream>
+#include <vector>
 
 class StringUtils {
+    private:
+        StringUtils();
+
+    public:
+
+        /// Function that splits a string using the delimiter provided
+        /// @param: str
+        /// @param: sep
+        /// @returns: a vector of strings containing substrings between the occurences of the separator
+        static std::vector<std::string> split(std::string str, std::string sep);
+
+        /// Function to convert all lower cases in a given string to upper case
+        /// @param: str
+        /// @returns: the given string with only upper case letters
+        static std::string to_upper(std::string str);
+
+
+        /// Function to remove whitespaces from both sides of a string
+        /// @param: str
+        /// @returns: the given string without spare whitespaces
+        static std::string trim(const std::string& str);
 
 };
 
